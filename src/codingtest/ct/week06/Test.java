ed07f2cef94fdfbaadd6e1f7afa3c3281ac1ea4d
rnurphy <ajvlzla0207@gmail.com>
2024-03-15 09:11:34 +0900

    6주차 누락분

--- src/codingtest/ct/week06/Test.java
+++ src/codingtest/ct/week06/Test.java
@@ -2,6 +2,7 @@ package codingtest.ct.week06;
 
 import java.util.Stack;
 
+
 public class Test {
 	public static void main(String[] args) {
 		int[][] board = { {0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1} };
